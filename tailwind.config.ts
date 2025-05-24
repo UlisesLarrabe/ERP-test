67c6cab2dbf563ca9e428430c3a273db8ecb12f3
Ulises <larrabeulises@gmail.com>
2025-05-24 16:43:16 -0300

    Feature: add colors variable

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        snow: "#fff9fb",
+        "eerie-black": "#252627",
       },
     },
   },
